6d2e883e2ac677e49165f634208b90e9398b6724
Vini2006 <vinijaneiro2006@gmail.com>
2024-05-10 17:17:09 -0300

    CommitSave

--- DiferencaData/Data.php
+++ DiferencaData/Data.php
@@ -1,11 +1,11 @@
 <?php 
     $DateTime = date("Y-m-d");
-    $datadenascimento = $_POST['obterdia'];
+    $datadenascimento = $_POST['DataNasc'];
 
     echo '<p>data atual:</p>'.$DateTime.'<br>';
-    echo  '<p>Sua data de nascimento:</p>'.$datadenascimento;
+    echo  '<p>Sua data de nascimento:</p>'.$DataNasc;
 
-    $diferenca = $DateTime = date_diff($datetime,$datadenascimento);
+    $diferenca = $DateTime = date_diff($Datetime,$DataNasc);
 
     echo '<h2>Anos</h2>' .$diferenca -> y. '<br>';
 ?>
