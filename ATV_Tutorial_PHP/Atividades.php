f77c0c2d7c2194a7bbeed0eea53ff355b5a6e34c
Vini2006 <janeirosouza26@gmail.com>
2024-05-14 16:30:48 -0300

    commitFIM+

--- ATV_Tutorial_PHP/Atividades.php
+++ ATV_Tutorial_PHP/Atividades.php
@@ -1,7 +1,35 @@
 <?php
-
-    /*phpinfo();*/
-
-    $texto01 + "Primeiro texto do iscript";
+    $texto01 = "Primeiro texto do iscript";
     print ($texto01);
+    echo '<br>';
+
+    //print ("texto02");
+    print('<span style="color:red">'.$texto01.'</span>');
+    //print ($texto02);
+    echo '<br>';
+
+    $raw = '22.11.1968';
+    $start = DateTime::createfromformat('d.m.Y',$raw);
+    echo "Data de Inicio: " .$start->format('Y-m-d'). "\n";
+
+    $end= clone $start;
+    $end->add(new DateInterval('P1M6D'));
+
+    $diff=$end->diff($start);
+    echo "Diferença: " .$diff-> format('%m mes, %d dias (toatal: %a dias)'). "\n";
+    //Diferença: 1 mês, 6 dias (total: 37 dias)
+    if($start < $end) {
+        echo "Começa antes do fim!\n";
+    }
+
+        // mostra todas as quintas-feiras entre $start e $end 
+        $periodInterval = DateInterval::createFromDateString('first thursday'); 
+        $periodIterator = new DatePeriod($start, $periodInterval, $end, DatePeriod::EXCLUDE_START_DATE);
+        
+        echo "<br>";
+
+        foreach($periodIterator as $date) {     
+            //mostra cada data no período     
+            echo $date->format('d-m-Y') . " "; 
+    }
 ?>
