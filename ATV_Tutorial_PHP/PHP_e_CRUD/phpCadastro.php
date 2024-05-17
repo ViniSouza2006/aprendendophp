a6306fb1a683449385bafb2316ee1639a04e0bfe
janeirosouza26@gmail.com <vinijaneiro2006@gmail.com>
2024-05-17 14:47:36 -0300

    CommitForm.PHP

--- ATV_Tutorial_PHP/PHP_e_CRUD/phpCadastro.php
+++ ATV_Tutorial_PHP/PHP_e_CRUD/phpCadastro.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CRUD com PHP, de forma simples e fácil</title>
 </head>
-<body>
-    
-</body>
+    <body>
+        <form action="">
+            <label for="">Nome: </label><br>
+            <input type="text" name="nome" placeholder="Digite seu Nome"><br><br>
+            <label for="">E-mail: </label><br>
+            <input type="email" name="email" placeholder="Digite seu Email"><br><br>
+            <label for="">Cidade: </label>
+            <input type="text" name="cidade" placeholder="Digite o Nome da Sua Cidade"><br><br>
+            <label for="">UF:</label>
+            <input type="text" name="" size="2" placeholder="UF"><br><br>
+            <input type="hidden" value="-1" name="id">
+            <button type="submit">Cadastrar</button>
+        </form>
+    </body>
 </html>
