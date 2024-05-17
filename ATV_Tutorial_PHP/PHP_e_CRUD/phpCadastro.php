967f3708df18e6e1ec8830dd9d08cc81c3498d8f
janeirosouza26@gmail.com <vinijaneiro2006@gmail.com>
2024-05-17 16:33:48 -0300

    Commit+

--- ATV_Tutorial_PHP/PHP_e_CRUD/phpCadastro.php
+++ ATV_Tutorial_PHP/PHP_e_CRUD/phpCadastro.php
@@ -6,7 +6,7 @@
     <title>CRUD com PHP, de forma simples e fácil</title>
 </head>
     <body>
-        <form action="">
+        <form action="<?=$_Server["PHP_SELF"]?>" method="POST">
             <label for="">Nome: </label><br>
             <input type="text" name="nome" placeholder="Digite seu Nome"><br><br>
             <label for="">E-mail: </label><br>
@@ -20,3 +20,46 @@
         </form>
     </body>
 </html>
+
+<?php
+    $obj_mysqli = new mysqli(127.0.0.1, "root", "", "tutocrudphp");
+
+    if ($obj_mysqli->connect_errno)
+    {
+        echo "Ocorrreu um erro na conexão com o banco de dados. ";
+        exit;
+    }
+
+    mysqli_set_charset($obj_mysqli, "utf8");
+
+
+    if(isset($_POST["nome"]) && isset($_POST["email"]) && isset($_POST[cidade]) && isset($_POST["uf"]))
+    {
+        if(empty($_POST["nome"]))
+            $erro = "Campo nome obrigatório";
+        else
+        {
+            if(empty($_POST["email"]))
+                $erro= "Campo e-mail obrigatório";
+        else
+        {
+            $nome = $_POST["nome"];
+            $email = $_POST["email"];
+            $cidade = $_POST["cidade"];
+            $uf = $_POST["uf"]
+
+            $stmt = $obj_mysqli->prepare("INSERT INTO 'cliente' ('nome', 'email','cidade', 'uf') VALUES (?,?,?,?)");
+            $stmt->bind_param('ssss', $nome, $email, $cidade, $uf);
+
+            if(!$stmt->execute())
+            {
+                $erro = $stmt->error;
+            }
+            else
+            {
+                $sucesso = "Dados cadastrados com sucesso!";
+            }
+        }
+        }
+    }
+?>
