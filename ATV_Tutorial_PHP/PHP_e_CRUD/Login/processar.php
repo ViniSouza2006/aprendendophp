d322e52c44d2c2f80f2691c3c8d9dcd7d7f44b30
janeirosouza26@gmail.com <vinijaneiro2006@gmail.com>
2024-05-24 17:23:26 -0300

    LoginAtividade

---
+++ ATV_Tutorial_PHP/PHP_e_CRUD/Login/processar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<?php
+    $conexao = mysqli_connect("Localhost", "root", "");
+    mysqli_select_db($conexao, "tutocrudphp");
+    ?>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $nome = $_POST ["nome"];
+    $user = $_POST ["user"];
+    $pass = $_POST ["pass"];
+
+    $inserir = "INSERT INTO usuario (id, nome, usuario, senha) VALUES (NULL, '$nome', '$user', '$pass');";
+    mysqli_query($conexao, $inserir) or die (mysqli_error($conexao));
+    echo"Você foi cadastrado com sucesso. Clique <a href='login.html'>Aqui</a> para fazer log-in.";
+    ?>
+</body>
+</html>
