89a9466bfff2ad7c4d2fad2aa33de0c9d4925dc7
Vini2006 <janeirosouza26@gmail.com>
2024-06-10 17:05:18 -0300

    +

--- CrudAtividade_2/index.php
+++ CrudAtividade_2/index.php
@@ -8,24 +8,4 @@ require_once 'header.php';
     </div>
     </div>
 <?php
-require_once 'connect.php';
-require_once 'header.php';
-?>
-<div class="container">
-    <?php
-    if(isset($_POST['addnew'])){
-        if(empty($_POST['firstname']) || empty($_POST['contact']) ){
-            echo "Please fillout all required fields";
-        }else{
-            $firstname = $_POST['firstname'];
-            $lastname = $_POST['lastname'];
-            $address = $_POST['address'];
-            $contact = $_POST['contact'];
-            $sql = "INSERT INTO user(firstname,lastname,address,contact)
-            VALUES('$firstname','$lastname','$address','$contact')";
-                if($con->query(sql)===TRUE){
-                    echo "<div class='alert alert-success'>Successfully added new user</div>";
-                }
-        }
-    }
-</div>
+require_once 'footer.php';
